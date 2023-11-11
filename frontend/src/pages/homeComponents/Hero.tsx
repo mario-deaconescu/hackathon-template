132fff44d3f143c1ee4824171b09a6b6144eb3c6
ioan-stefan-raducu <istefanraducu@gmail.com>
2023-11-11 22:19:41 +0200

    ProfileStudent Final Hardcoddat

--- frontend/src/pages/homeComponents/Hero.tsx
+++ frontend/src/pages/homeComponents/Hero.tsx
@@ -1,4 +1,4 @@
-import { Image } from '@nextui-org/react'
+import { Button, Image } from '@nextui-org/react'
 import HeroBg from '../../static-images/hero-bg.jpg'
 import React from 'react'
 
@@ -6,14 +6,18 @@ export default function Hero() {
   return (
     <div className="w-screen h-screen relative">
         <img src={HeroBg} alt="hero background" className='object-cover w-full h-full blur-sm brightness-50'/>
-        <div className="w-[50%] h-[40%] absolute top-1/2 left-0 transform -translate-y-1/2 rounded-3xl rounded-s-none bg-slate-800 shadow-2xl">
-            <div className='text-slate-100 text-3xl p-10 bold'>
+        <div className="w-[50%] h-[40%] absolute top-1/2 left-0 transform -translate-y-1/2 rounded-3xl rounded-s-none bg-slate-800 shadow-2xl border">
+            <div className='text-slate-100 text-3xl p-10 bold text-center'>
                 4tzaPoliUnibuc
             </div>
-            <div className="text-center text-slate-200">
+            <hr/>
+            <div className="text-center text-slate-200 text-xl p-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea impedit accusantium voluptatum corrupti nisi soluta vitae non reiciendis dolores incidunt laboriosam, modi eligendi quam necessitatibus blanditiis eos explicabo assumenda cum atque voluptates illo dolore rem? Sunt rem molestiae, voluptate placeat minus in aut beatae expedita tempore similique repudiandae officiis mollitia?
             </div>
+            
         </div>
+        <Button color="success" variant="shadow" size="lg" className='absolute left-[80%] top-[80%] text-slate-50 text-2xl p-10' >Start Learning</Button>
+        <Button color="secondary" variant="shadow" size="lg" className='absolute right-[80%] top-[80%] text-slate-50 text-2xl p-10' >Login</Button>
     </div>
   )
 }

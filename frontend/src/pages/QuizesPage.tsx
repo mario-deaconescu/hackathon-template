be756a50e258cd52cae99d073e105345d2fd7a40
mariodeaconescu <mariodeaconescu2003@gmail.com>
2023-11-11 18:05:57 +0200

    MainPage

--- frontend/src/pages/QuizesPage.tsx
+++ frontend/src/pages/QuizesPage.tsx
@@ -1,11 +1,13 @@
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {ISkill, SkillsService} from "../api";
 import {Select, Selection, SelectItem} from "@nextui-org/react";
+import Quiz from "../quiz/Quiz.tsx";
 
 const QuizesPage = () => {
     const [skills, setSkills] = useState<ISkill[]>([]);
     const [selectedSkill, setSelectedSkill] = useState<ISkill | null>(null);
     const [selectedChapters, setSelectedChapters] = useState<Selection>(new Set([]));
+    const [availableQuestions, setAvailableQuestions] = useState<number>(0);
     useEffect(() => {
         SkillsService.getAllSkills().then((response) => {
             setSkills(response);
@@ -14,12 +16,18 @@ const QuizesPage = () => {
     useEffect(() => {
         console.log(selectedSkill);
     });
+    useEffect(() => {
+        setAvailableQuestions(Array.from(selectedChapters).length * 10);
+    }, [selectedChapters]);
     const selectSkill = (selection: Selection) => {
         setSelectedSkill(skills.find((skill) => skill.name == Array.from(selection)[0]) || null);
     }
+    const quiz = useMemo(() => (
+        <Quiz chapters={Array.from(selectedChapters) as string[]} questionNumber={2}/>
+    ), [selectedChapters]);
     return (
-        <div className="w-full h-full">
-            <div className="flex flex-row w-fit justify-center items-center gap-2">
+        <div className="w-full h-full p-10 overflow-y-auto">
+            <div className="w-full lg:w-fit flex flex-row justify-center items-center gap-2 flex-wrap">
                 <Select
                     label="Skill"
                     className="w-80"
@@ -33,6 +41,7 @@ const QuizesPage = () => {
                 </Select>
                 {selectedSkill &&
                     <Select
+                        selectionMode="multiple"
                         label="Chapters"
                         className="w-80"
                         placeholder="Select Chapters"
@@ -44,6 +53,13 @@ const QuizesPage = () => {
                         ))}
                     </Select>
                 }
+                {selectedChapters &&
+                    <div className="w-80">
+                        {availableQuestions} questions available
+                    </div>}
+            </div>
+            <div className="p-5">
+                {Array.from(selectedChapters).length > 0 && quiz}
             </div>
         </div>
     );
